8e7cb8603b2d3f9cbcbd94ec1dd8cb197d0b3f4a
magitaka <takahamagitai@gmail.com>
2022-03-29 18:26:04 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
-  validate
+  validate :nickname, presence: true
+  validate :birthday, presence: true
+  validate :first_name, presence: true
+  validate :last_name, presence: true
+  validate :first_kana_name, presence: true
+  validate :last_kana_name, presence: true
 end
