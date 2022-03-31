4a79903960e6044a52eac7bd3feda28f241253f4
magitaka <takahamagitai@gmail.com>
2022-03-31 12:11:14 +0900

    FactoryBotとバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_kana_name, presence: true
-  validates :last_kana_name, presence: true
+         
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: {/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+    validates :birthday
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :first_kana_name, format: {with: /\A[\p{katakana} 一-&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい"}
+    validates :last_kana_name, format: {with: /\A[\p{katakana} 一-&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい"}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
-    it "nicknameが空だと登録できない" do
+    context '新規登録できるとき' do
+      it '問題なく登録できる' do
+        expect(@user).to be_valid
+      end
     end
-    it "emailが空では登録できない" do
+    context '新規登録できないとき' do
+      it "ニックネームが必須" do
+        @user.nickname = ''
+        @user.valid?
+        binding.pry
+      end
+      it "emailが空では登録できない" do
+      end
     end
   end
 end
