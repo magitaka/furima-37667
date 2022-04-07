3cf3b7ef5b5e8f3b489a13063881c1cd0894cfbc
magitaka <takahamagitai@gmail.com>
2022-04-07 14:53:18 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    if @item.current_user.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
