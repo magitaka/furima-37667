727563b583854ef9648682fe230fcc7da35ec797
magitaka <takahamagitai@gmail.com>
2022-04-07 17:40:10 +0900

    Revert "削除機能実装"
    This reverts commit 6e03824e307241ad82875e721ff814604efe80a8.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :user_match, only: [:edit]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -34,9 +34,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  def destroy
-    @item.destroy
-    redirect_to root_path
+  def destory
   end
 
   def user_match
